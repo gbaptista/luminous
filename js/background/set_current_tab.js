181e3f9b4ce9c576905287839452ee725d6c1e35
Guilherme Baptista <guilhermebaptistasilva@gmail.com>
2018-01-26 21:29:41 -0200

    fix fiferox security issues

--- js/background/set_current_tab.js
+++ js/background/set_current_tab.js
@@ -2,9 +2,13 @@ setInterval(function() {
   chrome.tabs.query({ currentWindow: true, active: true, lastFocusedWindow: true }, function(tabs) {
     if(tabs[0]) {
       current_tab = tabs[0];
-      chrome.tabs.sendMessage(
-        current_tab.id, { action: 'set_current_tab_id', current_tab: current_tab }
-      );
+
+      // Error: Could not establish connection. Receiving end does not exist.
+      if(/^http/.test(current_tab.url)) {
+        chrome.tabs.sendMessage(
+          current_tab.id, { action: 'set_current_tab_id', current_tab: current_tab }
+        );
+      }
     }
   })
 }, 0);
