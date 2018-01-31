e5b3aefc47845b36b488b6a064d6b72293f0a970
Guilherme Baptista <guilhermebaptistasilva@gmail.com>
2018-01-31 21:38:29 -0200

    improve icons and badges performance

--- js/background/update_badge.js
+++ js/background/update_badge.js
@@ -1,65 +1,57 @@
-var load_store_data_from_tab = function(tab_id, url) {
-  chrome.storage.local.get(tab_id, function(data) {
-    if(!data) { data = {}; }
+var load_store_data_from_tab = function(tab_id, url, update_badge = false, update_icon = false) {
+  if(update_badge) {
+    chrome.storage.local.get(tab_id, function(data) {
+      if(!data) { data = {}; }
 
-    if(data[tab_id] && data[tab_id]['badge']) {
-      chrome.browserAction.setBadgeText(
-        { text: data[tab_id]['badge']['text'].toString() }
-      );
+      if(data[tab_id] && data[tab_id]['badge']) {
+        chrome.browserAction.setBadgeText(
+          { text: data[tab_id]['badge']['text'].toString() }
+        );
 
-      var calls = parseInt(data[tab_id]['badge']['calls']);
+        var calls = parseInt(data[tab_id]['badge']['calls']);
 
-      chrome.browserAction.setBadgeBackgroundColor(
-        { color: background_color_for_badge(calls) }
-      );
-    } else {
-      chrome.browserAction.setBadgeText({text: ''});
-    }
-  });
+        chrome.browserAction.setBadgeBackgroundColor(
+          { color: background_color_for_badge(calls) }
+        );
+      } else {
+        chrome.browserAction.setBadgeText({text: ''});
+      }
+    });
+  }
 
-  chrome.storage.sync.get('options', function(sync_data) {
-    if(!sync_data['options']) sync_data['options'] = {};
-    if(!sync_data['options']['injection_disabled']) sync_data['options']['injection_disabled'] = {};
+  if(update_icon) {
+    chrome.storage.sync.get('options', function(sync_data) {
+      if(!sync_data['options']) sync_data['options'] = {};
+      if(!sync_data['options']['injection_disabled']) sync_data['options']['injection_disabled'] = {};
 
-    injection_disabled = sync_data['options']['injection_disabled'];
+      injection_disabled = sync_data['options']['injection_disabled'];
 
-    var a_element = document.createElement('a');
-    a_element.href = current_tab_url;
-    var domain = a_element.hostname;
+      var a_element = document.createElement('a');
+      a_element.href = current_tab_url;
+      var domain = a_element.hostname;
 
-    if(injection_disabled['general'] || injection_disabled[domain]) {
-      chrome.browserAction.setIcon({
-        path: {
-          16: 'images/icons/16-gray.png',
-          20: 'images/icons/20-gray.png',
-          24: 'images/icons/24-gray.png',
-          32: 'images/icons/32-gray.png',
-          48: 'images/icons/48-gray.png',
-          64: 'images/icons/64-gray.png',
-          128: 'images/icons/128-gray.png',
-          256: 'images/icons/256-gray.png',
-          512: 'images/icons/512-gray.png'
-        }
-      });
-    } else {
-      chrome.browserAction.setIcon({
-        path: {
-          16: 'images/icons/16.png',
-          20: 'images/icons/20.png',
-          24: 'images/icons/24.png',
-          32: 'images/icons/32.png',
-          48: 'images/icons/48.png',
-          64: 'images/icons/64.png',
-          128: 'images/icons/128.png',
-          256: 'images/icons/256.png',
-          512: 'images/icons/512.png'
-        }
-      });
-    }
-  });
+      if(injection_disabled['general'] || injection_disabled[domain]) {
+        chrome.browserAction.setIcon({
+          path: {
+            16: 'images/icons/16-gray.png',
+            32: 'images/icons/32-gray.png',
+            64: 'images/icons/64-gray.png'
+          }
+        });
+      } else {
+        chrome.browserAction.setIcon({
+          path: {
+            16: 'images/icons/16.png',
+            32: 'images/icons/32.png',
+            64: 'images/icons/64.png'
+          }
+        });
+      }
+    });
+  }
 }
 
-var load_stored_data = function(changes) {
+var load_stored_data = function(update_badge = false, update_icon = false, changes) {
   chrome.tabs.query({ currentWindow:true, active: true, lastFocusedWindow: true }, function(tabs) {
       var current_tab_id = 'x';
 
@@ -67,16 +59,28 @@ var load_stored_data = function(changes) {
         current_tab_id = tabs[0].id.toString();
         current_tab_url = tabs[0].url;
 
-        load_store_data_from_tab(current_tab_id, current_tab_url);
+        if(changes && changes[current_tab_id]) {
+          update_badge = true;
+        }
+
+        if(changes && changes['options'] && changes['options'] && changes['options'].newValue['injection_disabled']) {
+          update_icon = true;
+        }
+
+        load_store_data_from_tab(
+          current_tab_id, current_tab_url, update_badge, update_icon
+        );
       }
     }
   )
 };
 
-chrome.tabs.onActivated.addListener(function (_activeInfo) { load_stored_data(); });
-
-chrome.storage.onChanged.addListener(function(changes, namespace) {
-  load_stored_data(changes);
+chrome.tabs.onActivated.addListener(function (_activeInfo) {
+  load_stored_data(true, true);
 });
 
-load_stored_data();
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  load_stored_data(false, false, changes);
+});
+
+load_stored_data(true, true);
