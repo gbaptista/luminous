43b936cd73098f765580c0428e952f2051c69f14
Guilherme Baptista <guilhermebaptistasilva@gmail.com>
2018-02-03 09:14:51 -0200

    remove closed tabs data from local storage

--- js/background/set_current_tab.js
+++ js/background/set_current_tab.js
@@ -13,12 +13,18 @@ var reset_counters_for_tab = function(tab_id) {
     if(!data_to_write) { data_to_write = {}; }
     if(!data_to_write[tab_id]) { data_to_write[tab_id] = {}; }
 
-    data_to_write[tab_id]['counters'] = {};
-    data_to_write[tab_id]['badge'] = { 'text': '', 'calls': 0 };
-
-    chrome.storage.local.set(data_to_write);
+    if(data_to_write[tab_id]['badge']) {
+      data_to_write[tab_id]['counters'] = {};
+      data_to_write[tab_id]['badge'] = { 'text': '', 'calls': 0 };
+      chrome.storage.local.set(data_to_write);
+    }
   });
+}
+
+var remove_data_for_tab = function(tab_id) {
+  chrome.storage.local.remove(tab_id.toString());
 }
 
 chrome.tabs.onCreated.addListener(function(tab) { reset_counters_for_tab(tab.id); });
 chrome.tabs.onUpdated.addListener(function(tab_id) { reset_counters_for_tab(tab_id); });
+chrome.tabs.onRemoved.addListener(function(tab_id) { remove_data_for_tab(tab_id); });
