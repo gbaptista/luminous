7a3302a4a650aa3a62953ff5b5493ebffbddf081
Guilherme Baptista <guilhermebaptistasilva@gmail.com>
2018-02-05 22:48:21 -0200

    build 0.0.11

--- js/content/interceptor.js
+++ js/content/interceptor.js
@@ -140,7 +140,7 @@ EventTarget.prototype.removeEventListener = function(type, listener, options) {
     super_this, type, listener, options
   );
 
-  if(removeEventListener_alias[type][listener]) {
+  if(removeEventListener_alias[type] && removeEventListener_alias[type][listener]) {
     var value_to_return_b = original_EventTarget_removeEventListener.call(
       super_this, type, removeEventListener_alias[type][listener], options
     );
