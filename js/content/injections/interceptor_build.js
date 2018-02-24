3b13cbdc52a9c64fa37675655655bb4e926b1644
Guilherme Baptista <guilhermebaptistasilva@gmail.com>
2018-02-23 22:18:48 -0300

    update note for reviewers

--- js/content/injections/interceptor_build.js
+++ js/content/injections/interceptor_build.js
@@ -1,16 +1,13 @@
 injections_controller(function() {
 
  /*
-  * Disclaimer for reviewers:
+  * Note for reviewers:
   *
   * The non-minified version for the code below is available at:
-  * https://github.com/gbaptista/luminous/blob/0.0.18/js/content/interceptor.js
   *
-  * This version was generated with the uglifyjs project
-  * from the following command:
-  * > uglifyjs -c -m -- js/content/interceptor.js
+  * - https://github.com/gbaptista/luminous/blob/0.0.18/js/content/interceptor.js
+  * - https://github.com/gbaptista/luminous/blob/0.0.18/js/content/interceptors/
   *
-  * This was done for performance reasons.
   */
   var content = 'UGLIFYJS_RESULT';
 
