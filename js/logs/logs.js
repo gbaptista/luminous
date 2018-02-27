d73de815658e5f90370f6a15d27e75752e9d9779
Guilherme Baptista <guilhermebaptistasilva@gmail.com>
2018-02-26 22:14:38 -0300

    logs templates

--- js/logs/logs.js
+++ js/logs/logs.js
@@ -1,29 +1,25 @@
 $(document).ready(function() {
-  chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    if(message.action == 'log_input') {
-      var row = Mustache.render(
-        '<tr class="{{result}}">' +
-          '<td>{{tab}}</td>' +
-          '<td>{{domain}}</td>' +
-          '<td>{{time}}</td>' +
-          '<td>{{kind}}</td>' +
-          '<td>{{type}}</td>' +
-          '<td>{{target}}</td>' +
-          '<td>{{code}}</td>' +
-        '</tr>',
-        {
-          tab: message.tab_id,
-          domain: message.domain,
-          time: short_time(message.data.time),
-          kind: message.data.kind,
-          type: message.data.type,
-          target: message.data.details.target,
-          code: message.data.details.code,
-          result: message.data.result
-        }
-      );
+  load_template('html/logs/templates/form.html', function(form_template) {
+    load_template('html/logs/templates/row.html', function(row_template) {
 
-      $('tbody').prepend(row);
-    }
+      $('#form').html(Mustache.render(form_template, {
+        title: 'lorem'
+      }));
+
+      chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+        if(message.action == 'log_input') {
+          $('tbody').prepend(Mustache.render(row_template, {
+              tab: message.tab_id,
+              domain: message.domain,
+              time: short_time(message.data.time),
+              kind: message.data.kind,
+              type: message.data.type,
+              target: message.data.details.target,
+              code: message.data.details.code,
+              result: message.data.result
+          }));
+        }
+      });
+    });
   });
 });
