f0be26221cf089e8173f4fa524a2fa20a3fed87c
gbaptista <guilhermebaptistasilva@gmail.com>
2019-03-22 22:33:57 -0300

    build 0.0.27

--- js/utils/injections/interceptor_build.js
+++ js/utils/injections/interceptor_build.js
@@ -5,8 +5,8 @@ var load_interceptor_element = function(from, callback_function) {
   *
   * The non-minified version for the code below is available at:
   *
-  * - https://github.com/gbaptista/luminous/blob/0.0.26/js/content/interceptor.js
-  * - https://github.com/gbaptista/luminous/blob/0.0.26/js/content/interceptors/
+  * - https://github.com/gbaptista/luminous/blob/0.0.27/js/content/interceptor.js
+  * - https://github.com/gbaptista/luminous/blob/0.0.27/js/content/interceptors/
   *
   */
   var content = 'UGLIFYJS_RESULT';
